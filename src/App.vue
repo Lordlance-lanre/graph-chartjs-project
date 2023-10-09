3ba88f6e18d73e887ebbc0febed04e4646574ee8
Lordlance-lanre <omosehinolanrewaju@gmail.com>
2023-10-09 15:41:24 +0100

    chart repo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Graph from './components/Graph.vue';
+</script>
+
+<template>
+  <header>
+    <div class="">
+      <Graph/>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Graph.vue
@@ -0,0 +1,96 @@
+<template>
+<div>
+	<p class="text-center mt-2 text-3xl font-bold animate-pulse text-indigo-600">Graph Project</p>
+	<canvas class="mt-24 md:mt-5" id="myChart"></canvas>
+</div>
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto';
+import {onMounted} from 'vue'
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40, 45, 89, 53, 38, 77],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 106, 237, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  },
+  {
+    label: 'My Second Dataset',
+    data: [45, 79, 40, 71, 86, 95, 60, 25, 59, 73, 48, 87],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 106, 237, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+
+onMounted(() =>{
+const myChart = new Chart(
+	document.querySelector('#myChart'),
+	config
+	)
+})
+</script>
